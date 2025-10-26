14041dec438411cc4707107ba9d38c9bd5d4d763
ZuZanaZZ <zuzanazemlicka@gmail.com>
2025-10-26 16:44:11 +0100

    Testing out gitignore.

--- obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6563710c4a7854efffa093f3ea6c35108ca8f05a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f192336ec956234712d955343c6888abd1cbf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
