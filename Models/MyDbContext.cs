47f9f8d58b6961ba75632986864eae8c1f36b86b
ZuZanaZZ <zuzanazemlicka@gmail.com>
2025-11-13 14:08:10 +0100

    Added index page for the to do tasks. Verified the functionality of the database.

--- Models/MyDbContext.cs
+++ Models/MyDbContext.cs
@@ -8,13 +8,6 @@ namespace ToDo.Models
 
         public DbSet<ToDoTask> ToDoTasks { get; set; }
     }
-
-    public class ToDoTask
-    {
-        public int Id { get; set; }
-        public bool Done { get; set; }
-        public required string task { get; set; }
-    }
 }
 
 
---
+++ Models/ToDoTask.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDo.Models
+{
+    public class ToDoTask
+    {
+        public int Id { get; set; }
+        public bool Done { get; set; }
+        public required string task { get; set; }
+
+        public ToDoTask()
+        {
+                    
+        }
+    }
+}
